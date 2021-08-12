8327257198f844c9ecdcd6698ba0bb24a918a472
unknown <jackymad06@gmail.com>
2021-08-12 22:41:44 +0700

    update

--- src/components/MusicBox/LeftBar.js
+++ src/components/MusicBox/LeftBar.js
@@ -1,27 +1,57 @@
-import { useContext } from "react";
-import Grid from "@material-ui/core/Grid";
-import Paper from "@material-ui/core/Paper";
-import Logo from "./Logo";
-import VideoCount from "./VideoCount";
-import Button from "@material-ui/core/Button";
-import { auth } from "../../firebase/config";
-import { AppContext } from "../../Context/AppProvider";
-import Avatar from "@material-ui/core/Avatar";
-import { AuthContext } from "../../Context/AuthProvider";
+import { useContext, useState } from 'react';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Logo from './Logo';
+import VideoCount from './VideoCount';
+import Button from '@material-ui/core/Button';
+import { auth } from '../../firebase/config';
+import { AppContext } from '../../Context/AppProvider';
+import Avatar from '@material-ui/core/Avatar';
+import { AuthContext } from '../../Context/AuthProvider';
 import { deleteDocument } from '../../firebase/services';
+import ModalComponent from './ModalComponent';
+import { CSSTransition } from 'react-transition-group';
+import Notification from "./Notification";
 
 const LeftBar = ({ classes, mainVideo }) => {
    const { videos, videoSelect } = useContext(AppContext);
    const { user } = useContext(AuthContext);
-
+   const [open, setOpen] = useState(false);
+   const [statusNotifiParent, setStatusNotifi] = useState({
+      open: false,
+      title: "",
+      type: "",
+   });
 
    const deleteVideo = () => {
-      console.log(videoSelect);
       videoSelect.forEach((id) => {
          deleteDocument('videos', id);
       });
+      handleCloseModal();
+      setStatusNotifi({
+         ...statusNotifiParent,
+         open: true,
+         title: "Delete video successfully !",
+         type: "success",
+      });
    };
 
+   const handleOpenModal = () => {
+      if(videoSelect.length === 0){
+         setStatusNotifi({
+            ...statusNotifiParent,
+            open: true,
+            title: "Please select video !",
+            type: "error",
+         });
+      } else {
+         setOpen(true);
+      }
+   };
+
+   const handleCloseModal = () => {
+      setOpen(false);
+   };
 
    return (
       <Grid item xs={3}>
@@ -55,11 +85,15 @@ const LeftBar = ({ classes, mainVideo }) => {
                variant="contained"
                color="primary"
                style={{ width: '100%' }}
-               onClick={deleteVideo}
+               onClick={handleOpenModal}
             >
                Delete Video
             </Button>
+            {
+               (open) ? <ModalComponent deleteVideo={deleteVideo} handleCloseModal={handleCloseModal} /> : false
+            }
          </Paper>
+         <Notification statusNotifiParent={statusNotifiParent} />
       </Grid>
    );
 };
